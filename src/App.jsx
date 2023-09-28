342a1166816667dc3d97c42a0cd35ab819ecf0fe
prynskaf <prince.kaf12@gmail.com>
2023-09-28 23:37:07 +0200

    files updated

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Darkmode from './components/Darkmode'
+import Main from './components/Main'
+
+function App() {
+
+
+  return (
+    <>
+     <Darkmode />
+     <Main />
+    </>
+  )
+}
+
+export default App
